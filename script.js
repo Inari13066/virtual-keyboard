9da3216ae532d89b052af6d07be5889eb0fa6ca0
Inari13066 <devil13066@gmail.com>
2023-04-20 14:59:48 +0300

    feat: add buttons actions

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import keys from "./keyboard.json" assert { type: "json" };
 
+let btnColor = "#538083";
+
 const body = document.querySelector("body");
 
 let header = document.createElement("h1");
@@ -10,7 +12,9 @@ let text = document.createElement("textarea");
 let keyboardContainer = document.createElement("table");
 
 let notes = document.createElement("p");
-notes.innerHTML = 'Created in Windows<br>Press "alt" + "shift" to change the language';
+notes.innerHTML = 'Created in Windows. Press "alt" + "shift" to change the language';
+
+let noChangeBtns = [];
 
 body.append(header);
 body.append(text);
@@ -21,10 +25,61 @@ for (let i = 0; i < 5; i++) {
     keys[i].forEach((key) => {
         let btn = document.createElement("button");
         btn.innerHTML = key.name;
+        if (key.name.match(/[a-z]/i) && key.name.length == 1) {
+            btn.id = `${key.name}`;
+        } else {
+            btn.id = `${key.code}`;
+        }
+
         keyboardContainer.rows[i].append(btn);
-        btn.style.width = `${40*key.size}px`;
+        btn.style.width = `${40 * key.size}px`;
+        if (key.shift == "no") {
+            noChangeBtns.push(key.name);
+        }
+        let char = btn.innerHTML;
+        btn.addEventListener("click", function () {
+            console.log("here", char);
+            addChar(char);
+        });
     });
 }
 
 body.append(notes);
+text.focus();
 
+text.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    let btnPressed;
+    if (e.key.toLowerCase().match(/[a-z]/i) && e.key.length == 1) {
+        btnPressed = document.querySelector(`#${e.key.toLowerCase()}`);
+    } else {
+        btnPressed = document.querySelector(`#${e.code.toLowerCase()}`);
+    }
+
+    btnPressed.style.backgroundColor = btnColor;
+});
+
+text.addEventListener("keyup", function (e) {
+    console.log(e.key);
+    let btnPressed;
+    if (e.key.toLowerCase().match(/[a-z]/i) && e.key.length == 1) {
+        btnPressed = document.querySelector(`#${e.key.toLowerCase()}`);
+    } else {
+        btnPressed = document.querySelector(`#${e.code.toLowerCase()}`);
+    }
+
+    btnPressed.style.removeProperty("background-color");
+});
+
+function addChar(char) {
+    if (!noChangeBtns.includes(char)) {
+        console.log("here again", text, text.value);
+        text.value += char;
+    } else if (char == "space") {
+        text.value += char;
+    } else if (char == "backspace") {
+        text.value = text.value.slice(0, -1);
+    }
+    text.focus();
+    text.selectionStart = text.value.length;
+}

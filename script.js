5726e00962b66288af0bf8a276084a8d7afeb71e
Inari13066 <devil13066@gmail.com>
2023-04-28 21:00:27 +0300

    feat: add lang change

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-import keys from "./keyboard.json" assert { type: "json" };
+import keysEn from "./keyboard.json" assert { type: "json" };
+import keysRu from "./keyboardRu.json" assert { type: "json" };
 
 let btnColor = "#538083";
+let mainLang = "en"
+let keys = keysEn;
 
 const body = document.querySelector("body");
 
@@ -8,59 +11,25 @@ let header = document.createElement("h1");
 header.innerHTML = "Virtual Keyboard";
 
 let text = document.createElement("textarea");
+text.autofocus;
 
 let keyboardContainer = document.createElement("table");
 
 let notes = document.createElement("p");
-notes.innerHTML = 'Created in Windows. Press "alt" + "shift" to change the language. Double click to pinch "shift"';
+notes.innerHTML =
+    'Created in Windows. Press "alt" + "shift" to change the language. Double click to pinch "shift" or "alt"';
 
 let noChangeBtns = [];
 let isShiftPressed = false;
+let isAltPressed = false;
 
 body.append(header);
 body.append(text);
 body.append(keyboardContainer);
-for (let i = 0; i < 5; i++) {
-    let row = document.createElement("tr");
-    keyboardContainer.append(row);
-    keys[i].forEach((key) => {
-        let btn = document.createElement("button");
-        btn.innerHTML = key.name;
-        if (key.name.match(/[a-z]/i) && key.name.length == 1) {
-            btn.id = `${key.name}`;
-        } else {
-            btn.id = `${key.code}`;
-        }
-
-        keyboardContainer.rows[i].append(btn);
-        btn.style.width = `${40 * key.size}px`;
-        if (key.shift == "no") {
-            noChangeBtns.push(key.name);
-            noChangeBtns.push(key.code);
-        }
-        if (key.name == "shift") {
-            btn.addEventListener("dblclick", function (e) {
-                handleShift();
-                console.log(e.srcElement.id);
-                let btnPressed = document.querySelector(`#${e.srcElement.id.toLowerCase()}`);
-                if (isShiftPressed) {
-                    btnPressed.style.backgroundColor = btnColor;
-                } else {
-                    btnPressed.style.removeProperty("background-color");
-                }
-            });
-        }
-        let char = btn.innerHTML;
-        btn.addEventListener("click", function () {
-            addChar(char);
-        });
-    });
-}
+createKeys("en");
 
 body.append(notes);
 text.focus();
-
-let btns = document.querySelectorAll("button");
 
 text.addEventListener("keydown", function (e) {
     let btnPressed;
@@ -92,30 +61,50 @@ text.addEventListener("keyup", function (e) {
     btnPressed.style.removeProperty("background-color");
 });
 
-function addChar(char) {
+function addChar(char, id) {
+    console.log(text.selectionStart);
+    let position = text.selectionStart;
+    let arr = text.value.split("");
+    //TODO: add from virtual keyboard where the cursor is
     if (!noChangeBtns.includes(char)) {
         if (!isShiftPressed) {
-            text.value += char;
+            arr.splice(position, 0, char);
+            position++;
         } else {
-            text.value += findInKeys(char).shift;
+            arr.splice(position, 0, findInKeys(char).shift);
+            position++;
         }
-
-    } else if (char == "space") {
-        text.value += " ";
-    } else if (char == "backspace") {
-        text.value = text.value.slice(0, -1);
-    } else if (char == "shift") {
+    } else if (id == "space") {
+        arr.splice(position, 0, " ");
+        position++;
+    } else if (id == "backspace") {
+        arr.splice(position - 1, 1);
+        position--;
+    } else if (id.includes("shift")) {
         // handleShift();
         // TODO: Handle short shift click
+        if (isAltPressed) {
+            changeLang();
+        }
+    } else if (id.includes("alt")) {
+        if (isShiftPressed) {
+            changeLang();
+        }
+    } else if (id == "enter") {
+        arr.splice(position, 0, "\n");
+        position++;
     }
+    text.value = arr.join("");
+    text.setSelectionRange(position, position);
     text.focus();
-    text.selectionStart = text.value.length;
 }
 
 function handleShift() {
+    let btns = document.querySelectorAll("button");
     console.log("handled");
     if (!isShiftPressed) {
         btns.forEach((b) => {
+            // console.log("handling", b);
             if (b.id.toLowerCase().match(/[a-z]/i) && b.id.length == 1) {
                 b.style.textTransform = "uppercase";
             } else if (!noChangeBtns.includes(b.id.toLowerCase())) {
@@ -148,6 +137,86 @@ function findInKeys(smth) {
         if (key != undefined) {
             return key;
         }
-    };
+    }
+}
 
+function changeLang() {
+    console.log("lang change");
+    if (mainLang == "en") {
+        mainLang = "ru";
+        createKeys("ru")
+    } else {
+        mainLang = "en";
+        createKeys("en")
+    }
+}
+
+function createKeys(lang) {
+    if (lang == "en") {
+        keys = keysEn;
+    } else {
+        keys = keysRu;
+    }
+
+    if (keyboardContainer.firstChild) {
+        removeAllChildNodes(keyboardContainer)
+    }
+
+    isAltPressed = false;
+    isShiftPressed = false;
+
+    for (let i = 0; i < 5; i++) {
+        let row = document.createElement("tr");
+        keyboardContainer.append(row);
+        keys[i].forEach((key) => {
+            let btn = document.createElement("button");
+            btn.innerHTML = key.name;
+            if (key.id.match(/[a-z]/i) && key.id.length == 1) {
+                btn.id = `${key.id}`;
+            } else {
+                btn.id = `${key.code}`;
+            }
+    
+            keyboardContainer.rows[i].append(btn);
+            btn.style.width = `${40 * key.size}px`;
+            if (key.shift == "no") {
+                noChangeBtns.push(key.id);
+                noChangeBtns.push(key.code);
+            }
+            if (key.id == "shift") {
+                btn.addEventListener("dblclick", function (e) {
+                    handleShift();
+                    let btnPressed = document.querySelector(`#${e.srcElement.id.toLowerCase()}`);
+                    if (isShiftPressed) {
+                        btnPressed.style.backgroundColor = btnColor;
+                    } else {
+                        btnPressed.style.removeProperty("background-color");
+                    }
+                });
+            }
+            if (key.id == "alt") {
+                btn.addEventListener("dblclick", function (e) {
+                    let btnPressed = document.querySelector(`#${e.srcElement.id.toLowerCase()}`);
+                    if (!isAltPressed) {
+                        btnPressed.style.backgroundColor = btnColor;
+                        isAltPressed = true;
+                    } else {
+                        btnPressed.style.removeProperty("background-color");
+                        isAltPressed = false;
+                    }
+                });
+            }
+            let char = btn.innerHTML;
+            let charID = btn.id;
+            btn.addEventListener("click", function () {
+                addChar(char, charID);
+            });
+        });
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
